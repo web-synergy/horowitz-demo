32cd04fb1db3ba2c0aa0d907826a7c79b0a84d78
Andrii <andriy30534>
2023-12-15 12:11:40 +0200

    rafact

--- horowitz/src/api/index.ts
+++ horowitz/src/api/index.ts
@@ -1,12 +1,17 @@
 import { client } from '../lib/client';
 import { PortableTextBlock } from '@portabletext/types';
 
+import { currentNewsQuery, homeQuery, newsQuery } from './query';
+import { QueryParams } from '@sanity/client';
+const DEFAULT_PARAMS = {} as QueryParams;
 export async function sanityFetch<T>(
-  draft: boolean,
-  query: string
+  query: string,
+  params = DEFAULT_PARAMS,
+  draft?: boolean
 ): Promise<T> {
   const secretToken = import.meta.env.VITE_SANITY_SECRET_TOKEN ?? '';
   const token = draft ? secretToken : '';
+
   if (draft && token === '') {
     throw new Error(
       'The `VITE_SANITY_SECRET_TOKEN` environment variable is required.'
@@ -17,41 +22,12 @@ export async function sanityFetch<T>(
 
   return client
     .withConfig({ token: token, perspective: perspective })
-    .fetch<T>(query);
+    .fetch<T>(query, params);
 }
+export const token = import.meta.env.VITE_SANITY_SECRET_TOKEN;
 
-export const getHomeData = async (language = 'ua') => {
-  return client.fetch(`*[_type == 'home'][0]{
-
-    'quote':quote{
-       'author': author[_key ==${language}'].value,
-         'quote':quote[_key =='${language}'].value,
-     },
-         
-      'banner':banner{
-      'title':  title[_key =='${language}'].value,
-       'dateEvent':dateEvent[_key =='${language}'].value,
-       'img':img.asset ->url
-
-      },
-    'winner':winner[]{
-    'name': name[_key =='${language}'].value,
-    'champion': champion[_key =='${language}'].value,
-      'img':img.asset ->url
-
-    },
-    'news':news[]->{
-      'description':description[_key =='${language}'].value,
-      'title':title[_key =='${language}'].value,
-      'img':img.asset->url
-
-    },
-    sponsors,
-    'videos':videos[]{
-      'title':title[_key =='${language}'].value,
-      link
-    },
-}`);
+export const getHomeData = async (language: string, draft?: boolean) => {
+  return sanityFetch(homeQuery, { language }, draft);
 };
 
 export interface INews {
@@ -61,25 +37,23 @@ export interface INews {
   description: PortableTextBlock[];
   img: string;
 }
-export const getNews = async (lang: string) => {
-  return client.fetch<INews[]>(`*[_type=='news']{
-    _id, 
-    'slug':slug.current,
-    'description':description[_key =='${lang}'].value,
-    'title':title[_key =='${lang}'].value,
-    'img':img.asset->url
-  }`);
+export const getNews = async (language: string) => {
+  return client.fetch<INews[]>(newsQuery, { language });
 };
 
-export const getNewsBySlug = async (slug: string, draft: boolean) => {
-  const query = `*[_type =="news" && slug.current=="${slug}"]{
-    _id, 
-    'slug':slug.current,
-    'description':description[_key =='ua'].value,
-    'title':title[_key =='ua'].value,
-    'img':img.asset->url
-  }`;
-  const result = await sanityFetch<INews[]>(draft, query);
+export const getNewsBySlug = async (
+  slug: string,
+  draft: boolean,
+  language = 'ua'
+) => {
+  const result = await sanityFetch(
+    currentNewsQuery,
+    {
+      language,
+      slug,
+    },
+    draft
+  );
   console.log(result);
   return result[0];
 };
