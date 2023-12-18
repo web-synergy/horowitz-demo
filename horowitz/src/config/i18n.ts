ff0cd6ec25d719003a162d970a0fea1bd4eb2810
Anna Prutnik <prutnik.anna@gmail.com>
2023-12-18 12:37:04 +0200

    fix: language detect by default

--- horowitz/src/config/i18n.ts
+++ horowitz/src/config/i18n.ts
@@ -5,14 +5,41 @@ import enLocale from '../locales/en/translation.json';
 import uaLocale from '../locales/ua/translation.json';
 import { localStorageKey } from './constants';
 
+const customPath = {
+  name: 'fromPath',
+  lookup() {
+    let found;
+    if (typeof window !== 'undefined') {
+      const location = window.location.pathname;
+      if (location !== '/') {
+        found = location.slice(1, 3);
+      }
+    }
+    return found;
+  },
+};
+
+const defaultLang = {
+  name: 'defaultLang',
+  lookup() {
+    return 'ua';
+  },
+};
+
+const languageDetector = new LanguageDetector();
+
+languageDetector.addDetector(customPath);
+languageDetector.addDetector(defaultLang);
+
 i18n
   .use(initReactI18next)
-  .use(LanguageDetector)
+  .use(languageDetector)
   .init({
     interpolation: {
       escapeValue: false,
     },
     detection: {
+      order: ['fromPath', 'localStorage', 'defaultLang'],
       lookupLocalStorage: localStorageKey,
     },
     resources: {
