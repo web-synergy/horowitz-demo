cd0a2ae6be1267758b6ecf672e019c11407bca16
Anna Prutnik <prutnik.anna@gmail.com>
2023-12-27 12:05:43 +0200

    feature: add video in hero section

--- horowitz/src/components/Hero/Hero.tsx
+++ horowitz/src/components/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Typography } from '@mui/material';
 import { useHomeData } from '../../store';
 import { useTranslation } from 'react-i18next';
+import video from '../../assets/video-instagram (online-video-cutter.com).mp4';
 
 const Hero = () => {
   const { banner } = useHomeData();
@@ -11,55 +12,104 @@ const Hero = () => {
       {banner && (
         <Box
           sx={{
+            position: 'relative',
             display: 'flex',
             flexDirection: 'column',
-            backgroundImage: ` url(${banner.img})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backdropFilter: 'blur(2.5px)',
-            height: '780px',
-            paddingTop: '290px',
-          }}>
+            // backgroundImage: ` url(${banner.img})`,
+            // backgroundSize: 'cover',
+            // backgroundPosition: 'center',
+            // backdropFilter: 'blur(2.5px)',
+            height: 'calc(100vh - 170px)',
+            overflow: 'hidden',
+            // paddingTop: '290px',
+          }}
+        >
           {/* Контент для секції Hero */}
+          <Box
+            sx={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              zIndex: 5,
 
+              '& video': {
+                minWidth: '100%',
+                minHeight: '100%',
+                width: 'auto',
+                height: 'auto',
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+              },
+            }}
+          >
+            <video autoPlay loop muted>
+              <source src={video} type="video/mp4" />
+            </video>
+          </Box>
+          <Box
+            sx={{
+              position: 'absolute',
+              minWidth: '100%',
+              minHeight: '100%',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              zIndex: 10,
+
+              background:
+                'linear-gradient(90deg, rgba(24,24,23,0.9) 0%, rgba(73,73,73,0.9) 50%, rgba(24,24,23,0.9) 100%)',
+            }}
+          ></Box>
           <Box
             sx={{
               display: 'flex',
               flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'flex-start',
               gap: '24px',
               maxWidth: '760px',
-              margin: '0 auto',
-            }}>
+              position: 'relative',
+              zIndex: 100,
+              height: '100%',
+              marginLeft: '30px',
+            }}
+          >
             <Typography
-              variant='h5'
-              align='center'
+              variant="h5"
+              align="center"
               sx={{
                 color: '#F2F2F2',
                 fontSize: '24px',
                 fontWeight: 600,
                 lineHeight: 'normal',
-              }}>
+              }}
+            >
               {banner.dateEvent}
             </Typography>
             <Typography
-              variant='h2'
-              align='center'
+              variant="h2"
               sx={{
                 color: '#F2F2F2',
                 fontSize: '62px',
                 fontWeight: 600,
                 lineHeight: '80px',
                 textTransform: 'uppercase',
-              }}>
+              }}
+            >
               {banner.title}
             </Typography>
             <Box
               sx={{
                 display: 'flex',
                 justifyContent: 'center',
-              }}>
+              }}
+            >
               <Button
-                variant='contained'
+                variant="contained"
                 sx={{
                   fontSize: '18px',
                   fontWeight: 700,
@@ -72,11 +122,12 @@ const Hero = () => {
                   backgroundColor: '#F2F2F2',
                   textTransform: 'none',
                   borderRadius: '2px',
-                }}>
+                }}
+              >
                 {t('main.submitApp')}
               </Button>
               <Button
-                variant='outlined'
+                variant="outlined"
                 sx={{
                   fontSize: '18px',
                   fontWeight: 500,
@@ -89,7 +140,8 @@ const Hero = () => {
                   textTransform: 'none',
                   borderRadius: '2px',
                   border: ' 1.5px solid #F2F2F2',
-                }}>
+                }}
+              >
                 {t('main.supportProject')}
               </Button>
             </Box>
